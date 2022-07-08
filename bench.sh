2cb627668ce2b6d32842870df9e7f1c735b94c69
Sylvain Soliman <Sylvain.Soliman+git@gmail.com>
2022-07-08 14:14:53 +0200

    Complete (?)

--- bench.sh
+++ bench.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t trappist $(dirname -- ${BASH_SOURCE[0]})
-docker run -v $(pwd):/notebook/work trappist jupyter nbconvert --to notebook --output work/Benchmark.ipynb --execute Benchmark.ipynb
+docker run -v $(pwd):/notebook/work trappist jupyter nbconvert --to notebook --output work/Benchmark.ipynb --execute --allow-errors Benchmark.ipynb
